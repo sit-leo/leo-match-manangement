73f2cc293a4aaf2500ac2a3b21dfa760fd3804b1
Volku <Natchanon.minato@mail.kmutt.ac.th>
2019-04-19 03:02:35 +0700

    [ADD] add Match Controller

---
+++ src/main/java/app/leo/matchmanagement/controllers/MatchController.java
@@ -0,0 +1,22 @@
+package app.leo.matchmanagement.controllers;
+
+import app.leo.matchmanagement.models.Match;
+import app.leo.matchmanagement.services.MatchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MatchController {
+
+    @Autowired
+    private MatchService matchService;
+
+    @GetMapping(path = "matches/{matchId:[\\d]}")
+    public ResponseEntity<Match> getProductsByShopId(@PathVariable Long matchId) {
+        return new ResponseEntity<>(this.matchService.getMatchByMatchId(matchId), HttpStatus.OK);
+    }
+}
