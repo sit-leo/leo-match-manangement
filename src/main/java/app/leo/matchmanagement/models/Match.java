6a737b8c773ed57fc762c7c6c815bd5b8aaf59c1
Volku <Natchanon.minato@mail.kmutt.ac.th>
2019-04-19 12:48:57 +0700

    [UPDATE] add Match attribute (start_date,applicant_ranking_end_date,recruiter_ranking_end_date,summary_date)

--- src/main/java/app/leo/matchmanagement/models/Match.java
+++ src/main/java/app/leo/matchmanagement/models/Match.java
@@ -6,6 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.util.Date;
 
 @Entity
 @Table(name = "matches")
@@ -18,11 +19,27 @@ public class Match implements Serializable {
     @NotNull
     private String name;
 
+    private Date startDate;
+
+    private Date applicantRankingEndDate;
+
+    private Date recruiterRankingEndDate;
+
+    private Date summaryRankingEndDate;
+
     public Match() {
     }
 
     public Match(@NotNull String name) {
         this.name = name;
+    }
+
+    public Match(@NotNull String name, Date startDate, Date applicantRankingEndDate, Date recruiterRankingEndDate, Date summaryRankingEndDate) {
+        this.name = name;
+        this.startDate = startDate;
+        this.applicantRankingEndDate = applicantRankingEndDate;
+        this.recruiterRankingEndDate = recruiterRankingEndDate;
+        this.summaryRankingEndDate = summaryRankingEndDate;
     }
 
     public Long getId() {
@@ -39,5 +56,37 @@ public class Match implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getApplicantRankingEndDate() {
+        return applicantRankingEndDate;
+    }
+
+    public void setApplicantRankingEndDate(Date applicantRankingEndDate) {
+        this.applicantRankingEndDate = applicantRankingEndDate;
+    }
+
+    public Date getRecruiterRankingEndDate() {
+        return recruiterRankingEndDate;
+    }
+
+    public void setRecruiterRankingEndDate(Date recruiterRankingEndDate) {
+        this.recruiterRankingEndDate = recruiterRankingEndDate;
+    }
+
+    public Date getSummaryRankingEndDate() {
+        return summaryRankingEndDate;
+    }
+
+    public void setSummaryRankingEndDate(Date summaryRankingEndDate) {
+        this.summaryRankingEndDate = summaryRankingEndDate;
     }
 }
