ba8d960b015ab7197afa344749ed5bc34793d802
Keerati <keerati.jear@gmail.com>
2019-04-24 15:11:27 +0700

    [ADD] add example for get user that parsed by interceptor

--- src/main/java/app/leo/matchmanagement/controllers/MatchController.java
+++ src/main/java/app/leo/matchmanagement/controllers/MatchController.java
@@ -1,5 +1,6 @@
 package app.leo.matchmanagement.controllers;
 
+import app.leo.matchmanagement.dto.User;
 import app.leo.matchmanagement.models.Match;
 import app.leo.matchmanagement.services.MatchService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,7 +18,7 @@ public class MatchController {
     private MatchService matchService;
 
     @GetMapping(path = "matches/{matchId:[\\d]}")
-    public ResponseEntity<Match> getMatchByMatchId(@PathVariable Long matchId) {
+    public ResponseEntity<Match> getMatchByMatchId(@PathVariable Long matchId, @RequestAttribute("user") User user) {
         return new ResponseEntity<>(this.matchService.getMatchByMatchId(matchId), HttpStatus.OK);
     }
 }
