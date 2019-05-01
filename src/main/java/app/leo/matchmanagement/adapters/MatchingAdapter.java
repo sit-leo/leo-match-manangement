47695a45041e8ca0c0a59cedc153a94da2541470
REi <Natchanon.minato@mail.kmutt.ac.th>
2019-05-01 17:55:32 +0700

    [UPDATE]  update to use only one path for both role

--- src/main/java/app/leo/matchmanagement/adapters/MatchingAdapter.java
+++ src/main/java/app/leo/matchmanagement/adapters/MatchingAdapter.java
@@ -2,6 +2,7 @@ package app.leo.matchmanagement.adapters;
 
 import app.leo.matchmanagement.dto.ApplicantMatch;
 import app.leo.matchmanagement.dto.RecruiterMatch;
+import app.leo.matchmanagement.dto.UserMatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,54 +24,26 @@ public class MatchingAdapter {
 
     private Logger logger = LoggerFactory.getLogger(MatchingAdapter.class);
 
-    public List<Long> getMatchIdByApplicantId(String token){
-        List<Long> matchIdList = new ArrayList<>();
-        try{
-            RestTemplate restTemplate = new RestTemplate();
-            String url = String.format(matchingApiUrl + "/applicant/matches");
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("Authorization", token);
-            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-            headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
-            HttpEntity<String> entity = new HttpEntity<>(headers);
-            System.out.println(entity.getHeaders());
-            ResponseEntity<List<ApplicantMatch>> responseEntity =restTemplate.exchange(
-                url,
-                HttpMethod.GET,
-                entity,
-                new ParameterizedTypeReference<List<ApplicantMatch>>(){}
-            );
-            List<ApplicantMatch> applicantMatchList = responseEntity.getBody();
-            for(ApplicantMatch applicantMatch:applicantMatchList){
-                matchIdList.add(applicantMatch.getMatchId());
-            }
-        }catch (NullPointerException exception){
-            logger.warn(exception.getMessage());
-        }
-        return matchIdList;
-    }
-
-    public List<Long> getMatchIdByRecruiterId(String token){
-        System.out.println(token);
+    public List<Long> getMatchIdByUserId(String token){
         List<Long> matchIdList = new ArrayList<>();
         try {
             RestTemplate restTemplate = new RestTemplate();
-            String url = String.format(matchingApiUrl + "/recruiter/matches");
+            String url = String.format(matchingApiUrl + "/user/matches");
             HttpHeaders headers = new HttpHeaders();
             headers.add("Authorization", token);
             headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
             headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
             HttpEntity<String> entity = new HttpEntity<>(headers);
             System.out.println(entity.getHeaders());
-            ResponseEntity<List<RecruiterMatch>> responseEntity =restTemplate.exchange(
+            ResponseEntity<List<UserMatch>> responseEntity =restTemplate.exchange(
                 url,
                 HttpMethod.GET,
                 entity,
-                new ParameterizedTypeReference<List<RecruiterMatch>>(){}
+                new ParameterizedTypeReference<List<UserMatch>>(){}
             );
-            List<RecruiterMatch> recruiterMatchList = responseEntity.getBody();
-            for(RecruiterMatch recruiterMatch:recruiterMatchList){
-                matchIdList.add(recruiterMatch.getMatchId());
+            List<UserMatch> userMatchList = responseEntity.getBody();
+            for(UserMatch userMatch:userMatchList){
+                matchIdList.add(userMatch.getMatchId());
             }
         }catch (NullPointerException exception){
             logger.warn(exception.getMessage());
