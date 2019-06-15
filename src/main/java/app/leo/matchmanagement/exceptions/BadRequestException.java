bd53fbb22fd4c90e1ead4c1c67be3a2064bd14a9
Volku <Natchanon.minato@mail.kmutt.ac.th>
2019-06-16 00:07:16 +0900

    [ADD] Add Bad Request Exception and Http Exception

---
+++ src/main/java/app/leo/matchmanagement/exceptions/BadRequestException.java
@@ -0,0 +1,22 @@
+package app.leo.matchmanagement.exceptions;
+
+public class BadRequestException extends HttpException {
+    public BadRequestException() {
+    }
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+    public BadRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BadRequestException(Throwable cause) {
+        super(cause);
+    }
+
+    public BadRequestException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
