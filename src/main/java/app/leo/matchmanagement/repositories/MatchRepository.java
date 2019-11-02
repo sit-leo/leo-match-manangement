a65e537f7db78c61533259070160a5d381ed615c
Keerati <keerati.jear@gmail.com>
2019-11-03 05:29:20 +0700

    [ADD] add query to find last match in organization

--- src/main/java/app/leo/matchmanagement/repositories/MatchRepository.java
+++ src/main/java/app/leo/matchmanagement/repositories/MatchRepository.java
@@ -1,6 +1,9 @@
 package app.leo.matchmanagement.repositories;
 
-import app.leo.matchmanagement.models.Match;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,9 +11,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
-import java.util.Date;
-import java.util.List;
+import app.leo.matchmanagement.models.Match;
+import app.leo.matchmanagement.models.Organization;
 
 @Repository
 public interface MatchRepository extends JpaRepository<Match,Long> {
@@ -33,5 +35,5 @@ public interface MatchRepository extends JpaRepository<Match,Long> {
 
     Page<Match> findAllByOrganizationIdIn(List<Long> organizationIdList, Pageable pageable);
 
-
+    Match findTopByOrganizationOrderByIdDesc(Organization organization);
 }
