69c0abdfd1d5fd254ddce1f0f91f067086a5dcf8
Keerati <keerati.jear@gmail.com>
2019-11-03 05:28:59 +0700

    [UPDATE] change dto to have self attribute

--- src/main/java/app/leo/matchmanagement/dto/MatchDTO.java
+++ src/main/java/app/leo/matchmanagement/dto/MatchDTO.java
@@ -1,7 +1,94 @@
 package app.leo.matchmanagement.dto;
 
-import app.leo.matchmanagement.models.Match;
+import java.util.Date;
 
-public class MatchDTO extends Match {
+import javax.validation.constraints.NotNull;
 
+public class MatchDTO  {
+
+	private Long id;
+
+	@NotNull
+	private String name;
+
+	private String description;
+
+	@NotNull
+	private Date startJoiningDate;
+
+	@NotNull
+	private Date endJoiningDate;
+
+	@NotNull
+	private Date applicantRankingEndDate;
+
+	@NotNull
+	private Date recruiterRankingEndDate;
+
+	@NotNull
+	private Date announceDate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getStartJoiningDate() {
+		return startJoiningDate;
+	}
+
+	public void setStartJoiningDate(Date startJoiningDate) {
+		this.startJoiningDate = startJoiningDate;
+	}
+
+	public Date getEndJoiningDate() {
+		return endJoiningDate;
+	}
+
+	public void setEndJoiningDate(Date endJoiningDate) {
+		this.endJoiningDate = endJoiningDate;
+	}
+
+	public Date getApplicantRankingEndDate() {
+		return applicantRankingEndDate;
+	}
+
+	public void setApplicantRankingEndDate(Date applicantRankingEndDate) {
+		this.applicantRankingEndDate = applicantRankingEndDate;
+	}
+
+	public Date getRecruiterRankingEndDate() {
+		return recruiterRankingEndDate;
+	}
+
+	public void setRecruiterRankingEndDate(Date recruiterRankingEndDate) {
+		this.recruiterRankingEndDate = recruiterRankingEndDate;
+	}
+
+	public Date getAnnounceDate() {
+		return announceDate;
+	}
+
+	public void setAnnounceDate(Date announceDate) {
+		this.announceDate = announceDate;
+	}
 }
